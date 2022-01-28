4342d6a9efc09d8b25c29872c06d8687a591676a
Rafael Telles <rafast.telles@gmail.com>
2022-01-28 12:06:39 -0300

    commit inicial

---
+++ client/pages/auth/signout.js
@@ -0,0 +1,25 @@
+import Router from "next/router";
+import { useEffect } from "react";
+import useRequest from "../../hooks/use-request";
+
+const Signout = () => {
+
+  const { doRequest } = useRequest({
+    url: '/api/users/signout',
+    method: 'post',
+    body: {},
+    onSuccess: () => Router.push('/')
+  });
+
+  useEffect(() => {
+    doRequest().catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      Signing you out...
+    </div>
+  );
+}
+
+export default Signout;
---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import BuildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>
+}
+
+LandingPage.getInitialProps = async (context) => {
+  const client = BuildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
+
+export default LandingPage;
